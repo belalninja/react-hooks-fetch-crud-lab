9309304dc8dca6e4470a3d058cc1b1712e86907f
Belal Ahmad Abo Moailish <belalabomoalsh@gmail.com>
2023-12-20 16:46:00 +0200

    Done

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,28 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, updateQuestions }) {
   const { id, prompt, answers, correctIndex } = question;
+  const handleDelete = () => {
+    fetch(`http://localhost:4000/questions/${id}`, { method: "DELETE" }).then(
+      (res) => {
+        if (res.status === 200) {
+          updateQuestions(id);
+        }
+      }
+    );
+  };
+
+  function handleCorrectAnswer(e) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: e.target.value,
+      }),
+    });
+  }
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -15,9 +36,11 @@ function QuestionItem({ question }) {
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleCorrectAnswer}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,25 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, setQuestions }) {
+  function updateQuestions(id) {
+    const deletedQuestion = questions.findIndex((q) => q.id === id);
+    if (deletedQuestion !== -1)
+      setQuestions(questions.filter((q) => q.id !== id));
+  }
+
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questions.map((q) => (
+          <QuestionItem
+            question={q}
+            key={q.id}
+            updateQuestions={updateQuestions}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
